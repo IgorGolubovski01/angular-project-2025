3db250ae72397362f5fd8e4c9d404a88c429188d
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-07-08 23:01:45 +0200

    Search component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button'
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
