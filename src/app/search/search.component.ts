963457e04e3a79267fa81e0a07aa26d170e73b21
IgorGolubovski01 <igor.golubovski.21@singimail.rs>
2025-07-09 17:07:07 +0200

    added components and services

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -4,10 +4,12 @@ import {MatTableModule} from '@angular/material/table';
 import { FlightModel } from '../models/flight.model';
 import { FlightService } from '../../services/flight.service';
 import { MatButtonModule } from '@angular/material/button';
+import { UtilsService } from '../../services/utils.service';
+import { LoadingComponent } from "../loading/loading.component";
 
 @Component({
   selector: 'app-search',
-  imports: [MatTableModule, NgIf, MatButtonModule],
+  imports: [MatTableModule, NgIf, MatButtonModule, LoadingComponent],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
@@ -15,7 +17,7 @@ export class SearchComponent {
   displayedColumns: string[] = ['id', 'destination', 'flightNumber', 'scheduledAt','actions'];
   dataSource: FlightModel[] | null = null
 
-  public constructor(){
+  public constructor(public utils: UtilsService ){
     FlightService.getFlights(0,8)
     .then(rsp=>this.dataSource = rsp.data.content)
   }
